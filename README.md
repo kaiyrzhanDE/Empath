b627b2fa5fb382533462a62c39ef0af304dc50f3
veildc <kaiyrzhan.de@gmail.com>
2025-02-16 00:51:05 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#This is Empath, my Kotlin Multiplatform project targeting Android, iOS, Desktop.
+
---
+++ composeApp/src/iosMain/kotlin/kaiyrzhan/de/empath/MainViewController.kt
@@ -0,0 +1,5 @@
+package kaiyrzhan.de.empath
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
