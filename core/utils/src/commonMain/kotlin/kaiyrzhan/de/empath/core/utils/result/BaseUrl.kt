fce24b475eeb7cc25339a72999f7a9771a140c92
veildc <kaiyrzhan.de@gmail.com>
2025-05-04 23:29:44 +0500

    Removed baseUrl values from project

--- core/utils/src/commonMain/kotlin/kaiyrzhan/de/empath/core/utils/result/BaseUrl.kt
+++ core/utils/src/commonMain/kotlin/kaiyrzhan/de/empath/core/utils/result/BaseUrl.kt
@@ -1,6 +1,6 @@
 package kaiyrzhan.de.empath.core.utils.result
 
-public const val BASE_URL: String = "http://45.139.78.34/"
+public const val BASE_URL: String = "BASE_URL"
 
 private const val FILE_STORAGE_BASE_URL = "${BASE_URL}api/v1/file-storage"
 
